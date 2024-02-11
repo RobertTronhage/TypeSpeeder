c2bfe7ca0b564a35c589caa398b7f642fb547962
RobertTronhage <143345075+RobertTronhage@users.noreply.github.com>
2024-02-11 11:47:28 +0100

    started working on menu

---
+++ src/main/java/se/ju23/typespeeder/menu/Menu.java
@@ -0,0 +1,35 @@
+package se.ju23.typespeeder.menu;
+
+import org.springframework.stereotype.Component;
+import se.ju23.typespeeder.io.ConsoleIO;
+import se.ju23.typespeeder.io.IO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class Menu implements MenuService{
+
+    IO io = new ConsoleIO();
+
+    @Override
+    public List getMenuOption() {
+        List<String>options = new ArrayList<>();
+
+        options.add("0 - Exit");
+        options.add("1 - Play game");
+        options.add("2 - View your stats");
+        options.add("3 - View global leaderboard");
+        options.add("4 - Manage Players (Admin)");
+
+        return options;
+    }
+
+    @Override
+    public void displayMenu() {
+        List<String>options = getMenuOption();
+        for (String s : options){
+            io.addString(s);
+        }
+    }
+}
---
+++ src/main/java/se/ju23/typespeeder/menu/MenuService.java
@@ -0,0 +1,10 @@
+package se.ju23.typespeeder.menu;
+
+import java.util.List;
+
+public interface MenuService {
+    List getMenuOption();
+
+    void displayMenu();
+
+}
